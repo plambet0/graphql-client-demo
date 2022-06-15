443a387182cae74162f5fcee3b53f8f62ea99a09
Plamen Uzunov <plamen.uzunov@strypes.eu>
2022-06-15 23:13:00 +0300

    added errors in company form

--- src/components/CompanyForm.js
+++ src/components/CompanyForm.js
@@ -30,37 +30,84 @@ function CompanyForm({ handleClose, companyInput }) {
   const [isMainMember, setisMainMember] = useState(companyInput?.is_main_member || '');
   const [addCompany] = useMutation(CREATE_COMPANY);
   const [updateCompany] = useMutation(UPDATE_COMPANY);
+  const [formErrors, setFormErrors] = useState({});
+
+  const errorTexts = {
+    name: 'Name is required!',
+    companyType: 'Company type is required!',
+    mactivity: 'Market activity is required!',
+    is_main_member: 'Is main member is required!',
+    membership: 'Membership is required',
+    memberIndex: 'Member index is required'
+  };
 
   const handleSumbit = () => {
-    const input = {
-      name: companyName,
-      company_type_id: parseInt(companyType),
-      market_activity_id: parseInt(mktActivity),
-      member_index: Boolean(memberIndex),
-      is_main_member: Boolean(isMainMember),
-      membership_id: parseInt(membership)
+    let hasErrors = false;
+    const errors = {
+      name: null,
+      companyType: null,
+      marketActivity: null,
+      memberIndex: null,
+      isMainMember: null,
+      membership: null
     };
-    if (!companyInput) {
-      addCompany({
-        variables: {
-          input
-        }
-      });
-      handleClose();
+
+    if (!companyName || companyName.length === 0) {
+      hasErrors = true;
+      errors.name = errorTexts.companyType;
+    }
+    if (!companyType) {
+      hasErrors = true;
+      errors.companyType = errorTexts.name;
+    }
+    if (!mktActivity) {
+      hasErrors = true;
+      errors.marketActivity = errorTexts.mactivity;
+    }
+    if (!isMainMember) {
+      hasErrors = true;
+      errors.isMainMember = errorTexts.is_main_member;
+    }
+    if (!membership) {
+      hasErrors = true;
+      errors.membership = errorTexts.membership;
+    }
+    if (!memberIndex) {
+      hasErrors = true;
+      errors.memberIndex = errorTexts.memberIndex;
+    }
+    if (hasErrors) {
+      setFormErrors(errors);
     } else {
-      updateCompany({
-        variables: {
-          id: parseInt(companyInput.id),
-          input
-        }
-      });
-      handleClose();
+      const input = {
+        name: companyName,
+        company_type_id: parseInt(companyType),
+        market_activity_id: parseInt(mktActivity),
+        member_index: Boolean(memberIndex),
+        is_main_member: Boolean(isMainMember),
+        membership_id: parseInt(membership)
+      };
+      if (!companyInput) {
+        addCompany({
+          variables: {
+            input
+          }
+        });
+        handleClose();
+      } else {
+        updateCompany({
+          variables: {
+            id: parseInt(companyInput.id),
+            input
+          }
+        });
+        handleClose();
+      }
     }
   };
 
-
   return (
-    <Dialog id="new-company-dialog"  open={true}>
+    <Dialog id="new-company-dialog" open={true}>
       <DialogTitle
         id="form-dialog-title"
         style={{
@@ -94,6 +141,8 @@ function CompanyForm({ handleClose, companyInput }) {
               data-testid="company-name-input-label"
               label="Company Name (full)"
               name="companyName"
+              error={formErrors.name !== null}
+              helperText={formErrors.name ? formErrors.name : ''}
               value={companyName}
               onChange={(e) => {
                 setCompanyName(e.target.value);
@@ -108,6 +157,8 @@ function CompanyForm({ handleClose, companyInput }) {
                 data-testid="company-type-input-label"
                 label="Company Type "
                 name="companyType"
+                error={formErrors.companyType !== null}
+                helperText={formErrors.companyType ? formErrors.companyType : ''}
                 value={companyType}
                 onChange={(e) => {
                   setCompanyType(e.target.value);
@@ -123,6 +174,8 @@ function CompanyForm({ handleClose, companyInput }) {
                 data-testid="market-activity-input-label"
                 label="Market Activity "
                 name="mktActivity"
+                error={formErrors.marketActivity !== null}
+                helperText={formErrors.marketActivity ? formErrors.marketActivity : ''}
                 value={mktActivity}
                 onChange={(e) => {
                   setMktActivity(e.target.value);
@@ -138,6 +191,8 @@ function CompanyForm({ handleClose, companyInput }) {
                 data-testid="membership-id-input-label"
                 label="Membership "
                 name="membership"
+                error={formErrors.membership !== null}
+                helperText={formErrors.membership ? formErrors.membership : ''}
                 value={membership}
                 onChange={(e) => {
                   setMembership(e.target.value);
@@ -153,6 +208,8 @@ function CompanyForm({ handleClose, companyInput }) {
                 data-testid="memberIndex-input-label"
                 label="Member Index "
                 name="memberIndex"
+                error={formErrors.memberIndex !== null}
+                helperText={formErrors.memberIndex ? formErrors.memberIndex : ''}
                 value={memberIndex}
                 onChange={(e) => {
                   setmemberIndex(e.target.value);
@@ -168,6 +225,8 @@ function CompanyForm({ handleClose, companyInput }) {
                 data-testid="isMainMember-input-label"
                 label="Is Main Member "
                 name="isMainMember"
+                error={formErrors.isMainMember !== null}
+                helperText={formErrors.isMainMember ? formErrors.isMainMember : ''}
                 value={isMainMember}
                 onChange={(e) => {
                   setisMainMember(e.target.value);
